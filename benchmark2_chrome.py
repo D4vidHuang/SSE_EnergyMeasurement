daf47f24c145f2727657b6f3f08476f849a045b9
yongchenghuang <Y.Huang-51@student.tudelft.nl>
2025-02-15 23:36:21 +0100

    Add auto test methods for MacOS
    Add auto test methods for MacOS with chrome and Firefox support.

---
+++ benchmark2_chrome.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time
+
+options = webdriver.ChromeOptions()
+options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
+
+    try:
+        driver.get("https://dacris.github.io/jsmark/WebGLDemo.html")
+        print("Webpage opened. Waiting for 1 minute...")
+        time.sleep(60)
+
+    finally:
+        driver.quit()
+        print("Browser closed.")
